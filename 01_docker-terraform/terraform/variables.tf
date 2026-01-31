e54c815ee0a310fef7d27697401296272e520991
mandel-17 <lsyo6171@gmail.com>
2026-01-31 22:06:17 +0900

    Add terraform files

---
+++ 01_docker-terraform/terraform/variables.tf
@@ -0,0 +1,37 @@
+variable "credentials" {
+  description = "My Credentials"
+}
+
+
+variable "project" {
+  description = "Project"
+}
+
+variable "region" {
+  description = "Region"
+  #Update the below to your desired region
+  default     = "us-central1"
+}
+
+variable "location" {
+  description = "Project Location"
+  #Update the below to your desired location
+  default     = "US"
+}
+
+variable "bq_dataset_name" {
+  description = "My BigQuery Dataset Name"
+  #Update the below to what you want your dataset to be called
+  default     = "ny_taxi"
+}
+
+variable "gcs_bucket_name" {
+  description = "My Storage Bucket Name"
+  #Update the below to a unique bucket name
+  default     = "terraform-demo-terra-bucket-20260131-test"
+}
+
+variable "gcs_storage_class" {
+  description = "Bucket Storage Class"
+  default     = "STANDARD"
+}
